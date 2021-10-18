533aae0c585bb5b42e8052df71cd9e8c8492250d
abhinavtembulkar <abhinavtembulkar@gmail.com>
2021-10-19 02:13:32 +0530

    features added

---
+++ static/js/code.js
@@ -0,0 +1,200 @@
+// am4core.useTheme(am4themes_animated);
+// Themes end
+// Create map instance
+var chart = am4core.create("chartdiv", am4maps.MapChart);
+var interfaceColors = new am4core.InterfaceColorSet();
+// Set map definition
+chart.geodata = am4geodata_worldLow;
+// Set projection
+chart.projection = new am4maps.projections.Mercator();
+// Export
+chart.exporting.menu = new am4core.ExportMenu();
+// Zoom control
+chart.zoomControl = new am4maps.ZoomControl();
+
+// Data for general and map use
+var destinationCities = [
+    {
+        "id": "brussels",
+        "title": "Brussels",
+        "latitude": 50.8371,
+        "longitude": 4.3676
+    }
+];
+
+var originCities = [
+    {
+        "id": "reykjavik",
+        "title": "Reykjavik",
+        "destinations":destinationCities.map((ele)=>ele.id),
+        "latitude": 64.1353,
+        "longitude": -21.8952
+    }
+];
+
+
+// Default to London view
+//chart.homeGeoPoint = { "longitude": originCities[0].zoomLongitude, "latitude": originCities[0].zoomLatitude };
+//chart.homeZoomLevel = originCities[0].zoomLevel;
+
+var targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
+var planeSVG = "m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47";
+
+// Texts
+var labelsContainer = chart.createChild(am4core.Container);
+labelsContainer.isMeasured = false;
+labelsContainer.x = 80;
+labelsContainer.y = 27;
+labelsContainer.layout = "horizontal";
+labelsContainer.zIndex = 10;
+
+var plane = labelsContainer.createChild(am4core.Sprite);
+plane.scale = 0.15;
+plane.path = planeSVG;
+plane.fill = am4core.color("#cc0000");
+
+var title = labelsContainer.createChild(am4core.Label);
+title.text = "Flights from London";
+title.fill = am4core.color("#cc0000");
+title.fontSize = 20;
+title.valign = "middle";
+title.dy = 2;
+title.marginLeft = 15;
+
+// var changeLink = chart.createChild(am4core.TextLink);
+// changeLink.text = "Click to change origin city";
+// changeLink.isMeasured = false;
+
+// changeLink.events.on("hit", function() {
+//     console.log(3)
+//     if (currentOrigin == originImageSeries.dataItems.getIndex(0)) {
+//         showLines(originImageSeries.dataItems.getIndex(1));
+//     }
+//     else {
+//         showLines(originImageSeries.dataItems.getIndex(0));
+//     }
+// })
+
+// changeLink.x = 142;
+// changeLink.y = 72;
+// changeLink.fontSize = 13;
+
+
+// The world
+var worldPolygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+worldPolygonSeries.useGeodata = true;
+worldPolygonSeries.fillOpacity = 0.6;
+worldPolygonSeries.exclude = ["AQ"];
+
+// Origin series (big targets, London and Vilnius)
+var originImageSeries = chart.series.push(new am4maps.MapImageSeries());
+
+var originImageTemplate = originImageSeries.mapImages.template;
+
+originImageTemplate.propertyFields.latitude = "latitude";
+originImageTemplate.propertyFields.longitude = "longitude";
+originImageTemplate.propertyFields.id = "id";
+
+originImageTemplate.cursorOverStyle = am4core.MouseCursorStyle.pointer;
+originImageTemplate.nonScaling = true;
+originImageTemplate.tooltipText = "{title}";
+
+originImageTemplate.setStateOnChildren = true;
+originImageTemplate.states.create("hover");
+
+originImageTemplate.horizontalCenter = "middle";
+originImageTemplate.verticalCenter = "middle";
+
+var originHitCircle = originImageTemplate.createChild(am4core.Circle);
+originHitCircle.radius = 11;
+originHitCircle.fill = interfaceColors.getFor("background");
+
+var originTargetIcon = originImageTemplate.createChild(am4core.Sprite);
+originTargetIcon.fill = interfaceColors.getFor("alternativeBackground");
+originTargetIcon.strokeWidth = 0;
+originTargetIcon.scale = 1.3;
+originTargetIcon.horizontalCenter = "middle";
+originTargetIcon.verticalCenter = "middle";
+originTargetIcon.path = targetSVG;
+
+var originHoverState = originTargetIcon.states.create("hover");
+originHoverState.properties.fill = chart.colors.getIndex(1);
+
+// when hit on city, change lines
+// originImageTemplate.events.on("hit", function(event) {
+//     console.log(2)
+//     showLines(event.target.dataItem);
+// })
+
+// destination series (small targets)
+var destinationImageSeries = chart.series.push(new am4maps.MapImageSeries());
+var destinationImageTemplate = destinationImageSeries.mapImages.template;
+
+destinationImageTemplate.nonScaling = true;
+destinationImageTemplate.tooltipText = "{title}";
+destinationImageTemplate.fill = interfaceColors.getFor("alternativeBackground");
+destinationImageTemplate.setStateOnChildren = true;
+destinationImageTemplate.states.create("hover");
+
+destinationImageTemplate.propertyFields.latitude = "latitude";
+destinationImageTemplate.propertyFields.longitude = "longitude";
+destinationImageTemplate.propertyFields.id = "id";
+
+var destinationHitCircle = destinationImageTemplate.createChild(am4core.Circle);
+destinationHitCircle.radius = 7;
+destinationHitCircle.fillOpacity = 1;
+destinationHitCircle.fill = interfaceColors.getFor("background");
+
+var destinationTargetIcon = destinationImageTemplate.createChild(am4core.Sprite);
+destinationTargetIcon.scale = 0.7;
+destinationTargetIcon.path = targetSVG;
+destinationTargetIcon.horizontalCenter = "middle";
+destinationTargetIcon.verticalCenter = "middle";
+
+originImageSeries.data = originCities;
+destinationImageSeries.data = destinationCities;
+
+// // Line series
+var lineSeries = chart.series.push(new am4maps.MapLineSeries());
+lineSeries.mapLines.template.line.strokeOpacity = 0.5;
+
+chart.events.on("ready", function() {
+    console.log(1)
+    showLines(originImageSeries.dataItems.getIndex(0));
+})
+
+chart.events.on("datavalidated",(event)=>{
+    console.log('DONE',event)
+    showLines(originImageSeries.dataItems.getIndex(0));
+})
+
+
+var currentOrigin;
+
+function showLines(origin) {
+    
+    var dataContext = origin.dataContext;
+    var destinations = dataContext.destinations;
+    // clear old
+    lineSeries.mapLines.clear();
+    lineSeries.toBack();
+    worldPolygonSeries.toBack();
+
+    currentOrigin = origin;
+
+    if (destinations) {
+        for (var i = 0; i < destinations.length; i++) {
+            var line = lineSeries.mapLines.create();
+            line.imagesToConnect = [origin.mapImage.id, destinations[i]];
+        }
+    }
+
+    title.text = "Flights from " + dataContext.title;
+
+    chart.zoomToGeoPoint({ latitude: dataContext.zoomLatitude, longitude: dataContext.zoomLongitude }, dataContext.zoomLevel, true);
+}
+
+var graticuleSeries = chart.series.push(new am4maps.GraticuleSeries());
+graticuleSeries.mapLines.template.line.strokeOpacity = 0.05;
+
+// chart.series.
---
+++ static/js/index.js
@@ -0,0 +1,81 @@
+const eles = document.getElementById('data')
+const eled1 = document.getElementById('location1')
+const eled2 = document.getElementById('location2')
+var counter = 0
+var data = []
+var sorcname = {}
+var destname = {}
+var output = {}
+
+const credentials = {
+    "Authorization" : "Bearer 3tp7a5869mfm3y9y58kh3stx",
+    "Accept" : "application/json",
+    "Content-type" : "application/json"
+}
+
+
+const locationfetcher = (location,element,imagerender,flag=false) => {
+    const xhttp = new XMLHttpRequest()
+    // let output = {}
+    xhttp.onload = () =>{
+        let text = JSON.parse(xhttp.responseText)
+        let originx = text['AirportResource']['Airports']['Airport']['Position']['Coordinate']['Latitude']
+        let originy = text['AirportResource']['Airports']['Airport']['Position']['Coordinate']['Longitude'] 
+        let locationname = text['AirportResource']['Airports']['Airport']['Names']['Name'][0]['$']
+        element.innerText = originx + " " + originy + " " + locationname + " " + location
+
+        datalist = {
+            "id":new Date().getTime(),
+            "title":locationname,
+            "latitude":originx,
+            "longitude":originy,
+            "destinations": destinationImageSeries.data.map((ele)=>ele.id),
+        }
+
+        imagerender.data = [datalist]
+    }
+    
+    xhttp.open("GET",`https://api.lufthansa.com/v1/references/airports/${location}`,true)
+    
+    for (let key in credentials) {
+        // console.log(key,credentials[key])
+        xhttp.setRequestHeader(key,credentials[key])        
+    }
+    xhttp.send()
+
+    // return output
+}
+
+const datafetcher = () => {
+    const xhttp = new XMLHttpRequest()
+    xhttp.onload = async () =>{
+        data = JSON.parse(xhttp.responseText)
+        console.log('DATA FETCHED')
+    }
+    
+    xhttp.open("GET",'https://api.lufthansa.com/v1/flight-schedules/flightschedules/passenger?airlines=LH&startDate=10OCT21&endDate=16OCT21&daysOfOperation=1&timeMode=UTC',true)
+    
+    for (let key in credentials) {
+        // console.log(key,credentials[key])
+        xhttp.setRequestHeader(key,credentials[key])        
+    }
+    xhttp.send()
+}
+
+const changer = () => {
+    let origin = data[counter]["legs"][0]['origin']
+    let destination = data[counter]["legs"][0]['destination']
+    eles.innerText = "Source: " + origin+" Destination: "+destination
+    console.log(origin,destination)
+    
+    locationfetcher(destination,eled2,destinationImageSeries)
+    locationfetcher(origin,eled1,originImageSeries)
+    // counter++
+    counter = Math.floor(Math.random() * data.length)
+    
+    showLines(originImageSeries.dataItems.getIndex(0));
+    chart.validateData()
+}
+
+setInterval(changer,8000)
+datafetcher()
